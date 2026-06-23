3e57dc59ec0564d4694fe4b5e74cd38404d91ac3
Fk4111 <khanfaiyaz359@gmail.com>
2026-06-24 01:17:55 +0530

    Fix deployment issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser, getAllUsers, getProfile,
-  changePassword, updateProfile, uploadAvatar, } = require("../controllers/authController");
-const { protect, adminOnly } = require("../middlewares/authMiddleware");
-const uploadAvatarMiddleware = require("../middlewares/uploadAvatar");
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  getProfile,
+  changePassword,
+  updateProfile,
+  uploadAvatar,
+} = require("../controllers/authController");
 
+const {
+  protect,
+  adminOnly,
+} = require("../middlewares/authMiddleware");
 
+const uploadAvatarMiddleware =
+  require("../middlewares/uploadAvatar");
 
-
+// Auth Routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/users", protect, adminOnly, getAllUsers);
+
+// Users
+router.get(
+  "/users",
+  protect,
+  adminOnly,
+  getAllUsers
+);
+
+// Profile
 router.get(
   "/profile",
   protect,
   getProfile
 );
-
-router.patch(
-  "/change-password",
-  protect,
-  changePassword
-);
-
 
 router.put(
   "/profile",
@@ -32,9 +45,42 @@ router.put(
 );
 
 router.patch(
+  "/change-password",
+  protect,
+  changePassword
+);
+
+// Avatar Upload Debug Version
+router.patch(
   "/avatar",
   protect,
-  uploadAvatarMiddleware.single("avatar"),
+  (req, res, next) => {
+    uploadAvatarMiddleware.single("avatar")(
+      req,
+      res,
+      function (err) {
+        if (err) {
+          console.log(
+            "MULTER ERROR =>",
+            err
+          );
+
+          return res.status(500).json({
+            success: false,
+            message: err.message,
+            error: err,
+          });
+        }
+
+        console.log(
+          "MULTER SUCCESS =>",
+          req.file
+        );
+
+        next();
+      }
+    );
+  },
   uploadAvatar
 );
 
